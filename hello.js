74993537e03fb1a04f2bee734de0fe3404cd9ec3
Carly Langlois <cj.langlois.cl@gmail.com>
2021-01-20 16:20:54 -0600

    create sample assignment

---
+++ hello.js
@@ -0,0 +1,7 @@
+function hello() {
+	// TODO: change this string so that your program prints "Hello World!" 
+	// when you hit the replit run button
+	return "Hello!";
+}
+ 
+module.exports = hello;
